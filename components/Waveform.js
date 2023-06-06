b0a1460489c760a24efeb8493c574e73b887daad
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2023-06-05 23:15:25 -0400

    researching and setting up an enviromnent for transition

--- components/Waveform.js
+++ components/Waveform.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Waveform() {
+  const [waveform, setWaveform] = useState([12, 23, 25, 35, 32, 46, 43, 35]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.waveform}>Waveform to go here</Text>
+      {waveform.map((wave, idx) => {
+        <Text key={idx}>{wave}</Text>;
+      })}
     </View>
   );
 }
